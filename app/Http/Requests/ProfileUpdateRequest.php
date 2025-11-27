61eb18833d7265b069e486ceaaf07049d0129678
unknown <mat.heude@mariecurie-sio.fr>
2025-11-27 16:31:14 +0100

    modif model user

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -18,6 +18,9 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'name' => ['string', 'max:255'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
+            'adresse'=>['string','max:255'],
+            'codePostal'=>['string','max:5'],
+            'ville'=>['string','max:255'],
         ];
     }
 }
